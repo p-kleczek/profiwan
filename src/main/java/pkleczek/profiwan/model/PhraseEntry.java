aeb19600f7d0784d68cb4a55324c59bb7adabf70
Paweł Kłeczek <p.l.kleczek@gmail.com>
2013-11-06 18:45:21 +0100

    - generowanie powtórek

--- src/main/java/pkleczek/profiwan/model/PhraseEntry.java
+++ src/main/java/pkleczek/profiwan/model/PhraseEntry.java
@@ -2,10 +2,21 @@ package pkleczek.profiwan.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class PhraseEntry implements Serializable {
+
+	public static int MIN_REVISION_FREQUENCY = 30; // 1x / 30 dni
+	public static int MAX_REVISION_FREQUENCY = 2; // 1x / 2 dni
+	public static int MIN_CORRECT_STREAK = 3; // 3x
+	public static int FREQUENCY_DECAY = 2; // 2 dni za kazda poprawna powtorke
+
+	/**
+	 * blad => reset postepow we FREQ do podanego ulamka
+	 */
+	private static double MISTAKE_MULTIPLIER = 0.5;
 
 	/**
 	 * 
@@ -54,4 +65,52 @@ public class PhraseEntry implements Serializable {
 		this.revisions = revisions;
 	}
 
+	// TODO: testowac !!
+	public boolean isReviseNow() {
+		int freq = getRevisionFrequency();
+
+		Calendar calendar = Calendar.getInstance();
+		Date lastRevisionDate = revisions.get(revisions.size() - 1).date;
+		calendar.setTime(lastRevisionDate);
+		calendar.add(Calendar.DATE, freq);
+		Date nextRevisionDate = calendar.getTime();
+		Date today = Calendar.getInstance().getTime();
+
+		return today.after(nextRevisionDate);
+	}
+
+	public int getRevisionFrequency() {
+		int freq = MAX_REVISION_FREQUENCY;
+		int correctStreak = 0;
+		boolean isInitialStreak = false;
+
+		for (int i = 0; i < revisions.size(); i++) {
+			RevisionEntry e = revisions.get(i);
+
+			if (e.mistakes == 0) {
+				if (isInitialStreak) {
+					freq += FREQUENCY_DECAY;
+				}
+
+				correctStreak++;
+
+				if (!isInitialStreak && correctStreak == MIN_CORRECT_STREAK) {
+					isInitialStreak = true;
+					correctStreak = 0;
+				}
+			} else {
+				if (isInitialStreak) {
+					freq -= correctStreak * FREQUENCY_DECAY
+							* MISTAKE_MULTIPLIER;
+					
+					// clamp
+					freq = Math.min(freq, MIN_REVISION_FREQUENCY);
+					freq = Math.max(freq, MAX_REVISION_FREQUENCY);
+				}
+				correctStreak = 0;
+			}
+		}
+
+		return freq;
+	}
 }
---
+++ src/test/java/pkleczek/profiwan/model/PhraseEntryTest.java
@@ -0,0 +1,134 @@
+package pkleczek.profiwan.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import pkleczek.profiwan.model.PhraseEntry.RevisionEntry;
+
+public class PhraseEntryTest {
+
+	@Test
+	public void testIsReviseNow() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		re = new RevisionEntry();
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+		re.date = cal.getTime();
+		re.mistakes = 1;
+		pe.getRevisions().add(re);
+
+		assertTrue(pe.isReviseNow());
+	}
+
+	@Test
+	public void testIsReviseNowInitial() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		re = new RevisionEntry();
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -PhraseEntry.FREQUENCY_DECAY);
+		re.date = cal.getTime();
+		re.mistakes = 1;
+		pe.getRevisions().add(re);
+
+		assertFalse(pe.isReviseNow());
+	}
+
+	@Test
+	public void testGetRevisionsFrequencyNoStreak() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		for (int i = 0; i < PhraseEntry.MIN_CORRECT_STREAK; i++) {
+			re = new RevisionEntry();
+			cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+			re.date = cal.getTime();
+			re.mistakes = 0;
+			pe.getRevisions().add(re);
+		}
+
+		assertEquals(pe.getRevisionFrequency(),
+				PhraseEntry.MAX_REVISION_FREQUENCY);
+	}
+
+	@Test
+	public void testGetRevisionsFrequencyStreak() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		for (int i = 0; i < PhraseEntry.MIN_CORRECT_STREAK + 1; i++) {
+			re = new RevisionEntry();
+			cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+			re.date = cal.getTime();
+			re.mistakes = 0;
+			pe.getRevisions().add(re);
+		}
+
+		assertEquals(PhraseEntry.MAX_REVISION_FREQUENCY
+				+ PhraseEntry.FREQUENCY_DECAY, pe.getRevisionFrequency());
+	}
+
+	@Test
+	public void testGetRevisionsFrequencyNoStreakError() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		for (int i = 0; i < PhraseEntry.MIN_CORRECT_STREAK - 2; i++) {
+			re = new RevisionEntry();
+			cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+			re.date = cal.getTime();
+			re.mistakes = 0;
+			pe.getRevisions().add(re);
+		}
+
+		re = new RevisionEntry();
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+		re.date = cal.getTime();
+		re.mistakes = 1;
+		pe.getRevisions().add(re);
+
+		assertEquals(PhraseEntry.MAX_REVISION_FREQUENCY,
+				pe.getRevisionFrequency());
+	}
+
+	@Test
+	public void testGetRevisionsFrequencyStreakError() {
+		PhraseEntry pe = new PhraseEntry();
+		RevisionEntry re = null;
+		Calendar cal = null;
+
+		for (int i = 0; i < PhraseEntry.MIN_CORRECT_STREAK + 2; i++) {
+			re = new RevisionEntry();
+			cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+			re.date = cal.getTime();
+			re.mistakes = 0;
+			pe.getRevisions().add(re);
+		}
+
+		re = new RevisionEntry();
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -PhraseEntry.MIN_CORRECT_STREAK);
+		re.date = cal.getTime();
+		re.mistakes = 1;
+		pe.getRevisions().add(re);
+
+		assertEquals(PhraseEntry.MAX_REVISION_FREQUENCY
+				+ PhraseEntry.FREQUENCY_DECAY, pe.getRevisionFrequency());
+	}
+}
